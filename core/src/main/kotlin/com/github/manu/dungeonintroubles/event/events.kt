7547c793fc5792539a81da2410d54cf8e618a7aa
Manuel <manuelmat41@gmail.com>
2023-01-11 19:09:50 +0100

    feat: updated events.kt

--- core/src/main/kotlin/com/github/manu/dungeonintroubles/event/events.kt
+++ core/src/main/kotlin/com/github/manu/dungeonintroubles/event/events.kt
@@ -1,6 +1,8 @@
 package com.github.manu.dungeonintroubles.event
 
 import com.badlogic.gdx.maps.tiled.TiledMap
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
 import com.badlogic.gdx.scenes.scene2d.Event
 
 class MapChangeEvent(val map: TiledMap) : Event()
+class CollisionDespawnEvent(val cell: TiledMapTileLayer.Cell) : Event()
