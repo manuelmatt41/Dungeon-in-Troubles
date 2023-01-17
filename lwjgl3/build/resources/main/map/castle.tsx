2ea937e018889e5563d39edf97bcea6beadfe10e
Manuel <manuelmat41@gmail.com>
2023-01-17 20:20:24 +0100

    feat: add trap generator system and music system

--- lwjgl3/build/resources/main/map/castle.tsx
+++ lwjgl3/build/resources/main/map/castle.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="castle" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="dungeontileset-extended.png" width="1024" height="1024"/>
+ <image source="tiles/dungeontileset-extended.png" width="1024" height="1024"/>
  <tile id="506">
   <animation>
    <frame tileid="494" duration="300"/>
