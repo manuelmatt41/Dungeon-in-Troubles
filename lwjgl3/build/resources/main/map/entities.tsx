f2dee8e495a3a94883974fc269cb92835ec619bb
Manuel <manuelmat41@gmail.com>
2023-01-24 17:54:47 +0100

    feat: upgrade spawn system

--- lwjgl3/build/resources/main/map/entities.tsx
+++ lwjgl3/build/resources/main/map/entities.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="entities" tilewidth="23" tileheight="28" tilecount="3" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="entities" tilewidth="64" tileheight="64" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="16" height="28" source="entities/idle_00.png"/>
@@ -9,5 +9,8 @@
  </tile>
  <tile id="21">
   <image width="8" height="8" source="entities/run_00.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="entities/run_06.png"/>
  </tile>
 </tileset>
